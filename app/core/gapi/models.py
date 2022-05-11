340378b05ce88b12f04094cc11c58e9267dc6ee3
Taewon Kim <rlaxodnjs199@gmail.com>
2022-05-11 17:07:19 -0500

    Add singleton pattern classmethod to create instance

--- app/core/gapi/models.py
+++ app/core/gapi/models.py
@@ -1,5 +1,6 @@
 from datetime import date
 from pathlib import Path
+from typing import List
 
 
 class CTScan:
@@ -10,11 +11,11 @@ class CTScan:
         mrn: int,
         study_id: str,
         ctdate: date,
-        fu: str,
-        dcm_in_path: Path,
-        dcm_ex_path: Path,
+        fu: int,
+        dcm_path: Path,
+        deid_in_path: Path,
+        deid_ex_path: Path,
         row_index: int,
-        type=None,
     ) -> None:
         self.proj = proj
         self.subj = subj
@@ -22,6 +23,23 @@ class CTScan:
         self.mrn = mrn
         self.study_id = study_id
         self.fu = fu
-        self.dcm_in_path = dcm_in_path
-        self.dcm_ex_path = dcm_ex_path
+        self.dcm_path = dcm_path
+        self.deid_in_path = deid_in_path
+        self.deid_ex_path = deid_ex_path
         self.row_index = row_index
+        self.series = []
+
+    @classmethod
+    def from_gspread(cls, gspread_ct_scan: List, row_index: int):
+        return cls(
+            proj=gspread_ct_scan[0],
+            subj=gspread_ct_scan[1],
+            mrn=gspread_ct_scan[2],
+            study_id=gspread_ct_scan[3],
+            ctdate=gspread_ct_scan[4],
+            fu=gspread_ct_scan[5],
+            dcm_path=gspread_ct_scan[6],
+            deid_in_path=gspread_ct_scan[7],
+            deid_ex_path=gspread_ct_scan[8],
+            row_index=row_index,
+        )
